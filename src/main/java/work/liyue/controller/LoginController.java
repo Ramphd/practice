86a321834aef1a1ce45e452aeb03dca1107f1124
Yue <wadqse@qq.com>
2016-07-17 17:11:20 +0800

    ##user login

--- src/main/java/work/liyue/controller/LoginController.java
+++ src/main/java/work/liyue/controller/LoginController.java
@@ -8,8 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import work.liyue.aspect.LogAspect;
 import work.liyue.service.UserService;
+import work.liyue.util.PracticeUtil;
+
+import java.util.Map;
 
 /**
  * Created by hzliyue1 on 2016/7/16,0016.
@@ -23,11 +27,22 @@ public class LoginController {
     UserService userService;
 
     @RequestMapping(path = {"/reg/"}, method = {RequestMethod.GET, RequestMethod.POST})
-
+    @ResponseBody
     public String reg(Model model, @RequestParam("username") String username,
                       @RequestParam("password") String password,
                       @RequestParam(value = "remember", defaultValue = "0") int remember) {
+        try {
+            Map<String, Object> map =
+                    userService.register(username, password);
+            if (map.isEmpty()) {
+                return PracticeUtil.getJSONString(0, "reg OK!");
+            } else {
+                return PracticeUtil.getJSONString(1, map);
+            }
 
-        return null;
+        } catch (Exception e) {
+            logger.error("reg error" + e.getMessage());
+            return PracticeUtil.getJSONString(1, "reg error");
+        }
     }
 }

5612d0f2233bb61b87b4ff3f8dd733822cdef96b
Yue <wadqse@qq.com>
2016-07-22 01:56:14 +0800

    ##video2 31min

--- src/main/java/work/liyue/controller/MessageController.java
+++ src/main/java/work/liyue/controller/MessageController.java
@@ -1,19 +1,27 @@
 package work.liyue.controller;
 
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import work.liyue.dao.MessageDao;
+import work.liyue.model.HostHolder;
 import work.liyue.model.Message;
+import work.liyue.model.User;
+import work.liyue.model.ViewObject;
 import work.liyue.service.MessageService;
+import work.liyue.service.UserService;
 import work.liyue.util.PracticeUtil;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by hzliyue1 on 2016/7/21,0021, 16:59:39.
@@ -25,6 +33,56 @@ public class MessageController {
     MessageDao messageDao;
     @Autowired
     MessageService messageService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    HostHolder hostHolder;
+
+    @RequestMapping(path = {"/msg/list"}, method = {RequestMethod.GET})
+    public String conversationDetail(Model model) {
+        try {
+            int localUserId = hostHolder.getUser().getId();
+            List<ViewObject> conversations = new ArrayList<ViewObject>();
+            List<Message> conversationList = messageService.getConversationList(localUserId, 0, 10);
+            for (Message msg : conversationList) {
+                ViewObject vo = new ViewObject();
+                vo.set("conversation", msg);
+                int targetId = msg.getFromId() == localUserId ? msg.getToId() : msg.getFromId();
+                User user = userService.getUser(targetId);
+                vo.set("user", user);
+                vo.set("unread", messageService.getConvesationUnreadCount(localUserId, msg.getConversationId()));
+                conversations.add(vo);
+            }
+            model.addAttribute("conversations", conversations);
+        } catch (Exception e) {
+            logger.error("获取站内信列表失败" + e.getMessage());
+        }
+        return "letter";
+    }
+
+
+    @RequestMapping(path = {"/msg/detail"}, method = {RequestMethod.GET})
+    public String conversationDetail( @Param("conversationId") String conversationId,Model model) {
+        try {
+            List<Message> conversationList = messageService.getConversationDetail(conversationId, 0, 10);
+            List<ViewObject> messages = new ArrayList<>();
+            for (Message msg : conversationList) {
+                ViewObject vo = new ViewObject();
+                vo.set("message", msg);
+                User user = userService.getUser(msg.getFromId());
+                if (user == null) {
+                    continue;
+                }
+                vo.set("headUrl", user.getHeadUrl());
+                vo.set("userId", user.getId());
+                messages.add(vo);
+            }
+            model.addAttribute("messages", messages);
+        } catch (Exception e) {
+            logger.error("获取详情消息失败" + e.getMessage());
+        }
+        return "letterDetail";
+    }
 
     @RequestMapping(path = {"/msg/addMessage"}, method = {RequestMethod.POST})
     @ResponseBody

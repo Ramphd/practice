cd4230b67014b205e81495f73bfe3654ada50b22
Yue <wadqse@qq.com>
2016-07-24 00:43:56 +0800

    ##video 12612

---
+++ src/main/java/work/liyue/service/LikeService.java
@@ -0,0 +1,7 @@
+package work.liyue.service;
+
+/**
+ * Created by hzliyue1 on 2016/7/24 ,0:41.
+ */
+public class LikeService {
+}
---
+++ src/main/java/work/liyue/util/JedisAdapter.java
@@ -0,0 +1,83 @@
+package work.liyue.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.*;
+
+/**
+ * Created by hzliyue1 on 2016/7/24 ,0:25.
+ */
+@Service
+public class JedisAdapter implements InitializingBean {
+    private static final Logger logger = LoggerFactory.getLogger(JedisAdapter.class);
+    private JedisPool pool = null;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        pool = new JedisPool();
+    }
+
+    public long sadd(String key, String value) {
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.sadd(key, value);
+        } catch (Exception e) {
+            logger.error("发生异常" + e.getMessage());
+            return 0;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+    public long srem(String key, String value) {
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.srem(key, value);
+        } catch (Exception e) {
+            logger.error("发生异常" + e.getMessage());
+            return 0;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+    public boolean sismember(String key, String value) {
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.sismember(key, value);
+        } catch (Exception e) {
+            logger.error("发生异常" + e.getMessage());
+            return false;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+    public long scard(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.scard(key);
+        } catch (Exception e) {
+            logger.error("发生异常" + e.getMessage());
+            return 0;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    private Jedis getJedis() {
+        //return jedis;
+        return pool.getResource();
+    }
+}
